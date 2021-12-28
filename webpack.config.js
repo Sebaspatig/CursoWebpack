2275fd32b83d433117b5d7975bef9a973eedaee1
Sebaspatig <sebaspatig@gmail.com>
2021-12-28 11:08:34 -0500

    Image and Font loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		filename: "main.js",
+		assetModuleFilename: "assets/images/[hash][ext][query]",
 	},
 	resolve: {
 		extensions: [".js"],
@@ -25,6 +26,24 @@ module.exports = {
 				test: /\.css|.styl$/i,
 				use: [MiniCssExtractPlugin.loader, "css-loader", "stylus-loader"],
 			},
+			{
+				test: /\.png/,
+				type: "asset/resource",
+			},
+			{
+				test: /\.(woff|woff2)$/,
+				use: {
+					loader:"url-loader",
+					options: {
+						limit: 10000,
+						mimetype: "aplication/font-woff",
+						name: "[name].[ext]",
+						outputPath: "./assets/fonts/",
+						publicPath: "./assets/fonts/",
+						esModule: false,
+					}
+				},
+			},
 		],
 	},
 	plugins: [
@@ -38,7 +57,7 @@ module.exports = {
 			patterns: [
 				{
 					from: path.resolve(__dirname, "src", "assets/images"),
-					to: "assets/images"
+					to: "assets/images",
 				},
 			],
 		}),
